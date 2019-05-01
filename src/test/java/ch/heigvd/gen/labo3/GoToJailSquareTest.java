f2faa102b395755ee5907959b849b9afc27d19b1
jerozerbib <jerozerbib@gmail.com>
2019-05-01 10:19:15 +0200

    done

---
+++ src/test/java/ch/heigvd/gen/labo3/GoToJailSquareTest.java
@@ -0,0 +1,63 @@
+package ch.heigvd.gen.labo3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoToJailSquareTest {
+    @Test
+    void goToJailSquareShouldHaveAName() {
+        String name = "GoToJailSquare";
+
+        Square square = new GoToJail(name);
+
+        assertEquals(square.getName(), name);
+    }
+
+    @Test
+    void netWorthHasNotChanged() {
+        Board board = new Board();
+        Die[] dices = new Die[2];
+        Player player = new Player("Player", board, dices);
+
+        String name = "GoToJail";
+        Square square = new GoToJail(name);
+        int w = player.getNetWorth();
+        player.getPiece().setLocation(square);
+        square.landedOn(player);
+        int afterGo = player.getNetWorth();
+        assertEquals(afterGo, w);
+    }
+
+    @Test
+    void gotoJailSquareIsLandedOn() {
+        Board board = new Board();
+        Die[] dices = new Die[2];
+        Player player = new Player("Player", board, dices);
+
+        String name = "GoToJail";
+        Square square = new GoToJail(name);
+        player.getPiece().setLocation(square);
+        assertTrue(square.landedOn(player));
+
+    }
+
+    @Test
+    void playerIsInJail(){
+        Board board = new Board();
+        Die[] dices = new Die[2];
+        Player player = new Player("Player", board, dices);
+
+        String name = "GoToJail";
+        Square square = new GoToJail(name);
+        player.getPiece().setLocation(square);
+        square.landedOn(player);
+        Square test = player.getPiece().getLocation();
+
+        assertEquals(player.getPiece().getLocation().getName(), "jail");
+
+    }
+
+
+
+}
